33da7b76b60f601e486c6df830c5865a0348a1dd
yusuketakeda727 <77325226+yusuketakeda727@users.noreply.github.com>
2021-02-22 21:19:59 +0900

    activestorageの導入、バリデーションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,3 +2,10 @@ class ItemsController < ApplicationController
   def index
   end
 end
+
+
+private
+
+def item_params
+  params.require(:item).permit(:image).merge(user_id: current_user.id)
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+
+  belongs_to :user
+  # has_one :order
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :title
+    validates :text
+    validates :price
+    validates :category_id
+    validates :status_id
+    validates :cost_id
+    validates :prefecture_id
+    validates :response_id
+  end
 end
