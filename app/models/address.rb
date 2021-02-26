ded3f7be60e050ceaac0973bd6b9ce4cce76cdf3
yusuketakeda727 <77325226+yusuketakeda727@users.noreply.github.com>
2021-02-27 06:28:34 +0900

    バリデーション、アソシエーションの設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,11 @@
 class Address < ApplicationRecord
+  belongs_to :order
+
+  with_options presence: true do
+    post_cord
+    city
+    address
+    phone_number
+  end
+  validates :prefecture_id, numericality: { other_than: 1 }
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,12 @@
 class Order < ApplicationRecord
-  include ActiveModel::Model
-  attr_accessor :token
+  # include ActiveModel::Model
+  # attr_accessor :token
 
-  validates :price, presence: true
+  belongs_to :item
+  belongs_to :user
+  has_one :address
 
-  def save
-    # 各テーブルにデータを保存する処理を書く
-  end
+  # def save
+  #   # 各テーブルにデータを保存する処理を書く
+  # end
 end
