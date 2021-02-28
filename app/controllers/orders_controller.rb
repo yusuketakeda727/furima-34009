56eedfdfdf5fdf32868cd777d7596d768089cac4
yusuketakeda727 <77325226+yusuketakeda727@users.noreply.github.com>
2021-02-28 18:13:14 +0900

    ビューファイルの作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,21 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
   def index
-    
+    @order = Order.new
+    @item = Item.find(params[:format])
   end
+
   def create
-    
+    if @order.create
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+  
+  private
+
+  def order_params
+    params.require(:user_id, :item_id).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :user
   root to: "items#index"
   resources :items
-  resources :orders, only: [:index, :create]
+  resources :orders, only: [:index, :new, :create]
 end
