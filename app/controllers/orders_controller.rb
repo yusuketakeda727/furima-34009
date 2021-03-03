2ed35fffc92fdc23471cac593e1acad136ffbce5
yusuketakeda727 <77325226+yusuketakeda727@users.noreply.github.com>
2021-03-03 17:53:56 +0900

    クレジットの購入処理

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,17 +12,28 @@ class OrdersController < ApplicationController
     @order_address = OrderAddress.new(order_params)
     @item = Item.find(params[:format])
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
       render :new
     end
   end
-  
+
   private
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(
+      user_id: current_user.id, item_id: params[:format], token: params[:token]
+    )
   end
 
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+    amount: @item.price,
+    card: order_params[:token],
+    currency: 'jpy'
+    )
+  end
 end
