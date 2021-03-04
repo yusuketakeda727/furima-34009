05b2c3d991392483770ad6dd6545ef23a467c226
yusuketakeda727 <77325226+yusuketakeda727@users.noreply.github.com>
2021-03-04 15:49:20 +0900

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
   private
 
   def basic_auth
-    # authenticate_or_request_with_http_basic do |username, password|
-    #   username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-    # end
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 
   def configure_permitted_parameters
