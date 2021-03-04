427f6ee75e4a50519f8bbc3d4ab80642ccd49b5f
yusuketakeda727 <77325226+yusuketakeda727@users.noreply.github.com>
2021-03-05 07:28:46 +0900

    commit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,7 +7,6 @@ class OrdersController < ApplicationController
 
   def create
     @order_address = OrderAddress.new(order_params)
-    binding.pry
     if @order_address.valid?
       pay_item
       @order_address.save
